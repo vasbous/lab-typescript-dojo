8ffa2f5585a5c530242ed9f7ef6e71ce6bba98e9
Vassilis Bousbourelis <vasbousdev@gmail.com>
2025-04-25 00:10:27 +0200

    solved lab

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -1,5 +1,21 @@
-//
-// Iteration 2 | Functions
-//
+function calcMultiplication(a: number, b: number): number {
+  return a * b;
+}
 
+function isEven(n: number): boolean {
+  return n % 2 === 0;
+}
 
+function calcArrayAverage(numbersArr: number[]): number | string {
+  if (numbersArr.length === 0) {
+    return "Please provide an array with at least one element";
+  }
+
+  const total = numbersArr.reduce((acc, cur) => {
+    return acc + cur;
+  });
+
+  const average = total / numbersArr.length;
+
+  return average;
+}
